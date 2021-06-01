75536b76085a845efd9d9f91e2bf59a7715a5306
JinnyYi <jinnyyi@yunify.com>
2021-06-01 16:35:03 +0800

    Bump to go-storage 4.0.0

--- doc.go
+++ doc.go
@@ -3,4 +3,4 @@ Package webdav provided support for local file system.
 */
 package webdav
 
-//go:generate go run -tags tools github.com/aos-dev/go-storage/v3/cmd/definitions service.toml
+//go:generate go run -tags tools github.com/beyondstorage/go-storage/v4/cmd/definitions service.toml
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/beyondstorage/go-service-webdav
 
 go 1.15
 
-require github.com/aos-dev/go-storage/v3 v3.6.0
+require github.com/beyondstorage/go-storage/v4 v4.0.0
