261f6b35d8d276d297bc9c1baec2532fbc628b23
JinnyYi <82445294+JinnyYi@users.noreply.github.com>
2021-06-01 17:01:20 +0800

    Rename module, Bump go-storage and Implement GSP-87 Feature Gates (#2)
    * Rename module name
    
    * Bump to go-storage 4.0.0
    
    * Implement GSP-87 Feature Gates

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 /*
-Package example provided support for local file system.
+Package webdav provided support for local file system.
 */
 package webdav
 
-//go:generate go run -tags tools github.com/aos-dev/go-storage/v3/cmd/definitions service.toml
+//go:generate go run -tags tools github.com/beyondstorage/go-storage/v4/cmd/definitions service.toml
